8dc40c90b82f27af571b9fcbfb8a6362005102b2
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-05-30 22:25:32 -0700

    the teensiest of eval-functions.

--- src/skeem/core.clj
+++ src/skeem/core.clj
@@ -1,7 +1,14 @@
 (ns skeem.core
   (:gen-class))
 
+(def ops {'+ +, '- -, '/ /, '* *})
+
+(defn eval-skeem [exp & exps]
+  (cond
+    (and (list? exp) (ops (first exp)))
+      (apply (ops (first exp)) (rest exp))))
+
 (defn -main
   "This is where the skeeming happens..."
   [& args]
-  (println "Hello, World!"))
+  (println (eval-skeem '(+ 1 2 3))))
